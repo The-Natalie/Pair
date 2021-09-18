708726834c05f17f3f8643cdd5927fa720a7b8b0
The-Natalie <natalie.diana.hall@gmail.com>
2021-09-18 16:29:28 -0400

    Model completed

--- Pair/Pair/Models/Person.swift
+++ Pair/Pair/Models/Person.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 
-class Person {
+class Person: Codable {
+    var person: String
+    var uuid: String
     
+    init(person: String, uuid: String = UUID().uuidString) {
+        self.person = person
+        self.uuid = uuid
+    }
     
 }  // End of Class
+
+extension Person: Equatable {
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+    
+    
+}
